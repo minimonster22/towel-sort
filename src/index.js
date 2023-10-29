0f2778223c907be53a78c65cbc91fbcb794a3c21
minimonster22 <tamaramonstr@gmail.com>
2023-10-29 21:28:08 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix) {
+        return [];
+    }
+
+    const sortedArray = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            // If the row index is even, append the elements in the row from left to right.
+            sortedArray.push(...matrix[i]);
+        } else {
+            // If the row index is odd, append the elements in the row from right to left.
+            sortedArray.push(...matrix[i].reverse());
+        }
+    }
+
+    return sortedArray;
 }
